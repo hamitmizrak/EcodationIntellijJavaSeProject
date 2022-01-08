a546bcd0ed34386222fa3edf90ee94122a8da5cd
hamitmizrak <hamitmizrak@gmail.com>
2022-01-08 12:39:48 +0300

    Access modifier

--- src/com/ecodation/ders002/method/_01_Method.java
+++ src/com/ecodation/ders002/method/_01_Method.java
@@ -4,28 +4,31 @@ public class _01_Method {
     //Ctrl+Shift+A
 
     //1-) voidli parametresiz
-    public static void voidliParametresiz(){
+    public static void voidliParametresiz() {
         System.out.println("Voidli parametresiz");
     }
 
     //2-) voidli parametreli
-    public static void voidliParametreli(String data){
-        System.out.println("Voidsiz parametreli "+data);
+    public static void voidliParametreli(String data) {
+        System.out.println("Voidsiz parametreli " + data);
     }
 
     //3-) voidsiz parametresiz
-    public static String  voidsizParametresiz() {
+    public static String voidsizParametresiz() {
+
         return "Muhammet , Uğur ";
     }
+
     //4-) voidsiz parametresiz
-    public static String  voidsizParametreli(String data) {
+    public static String voidsizParametreli(String data) {
         return data;
     }
 
     //Overloading:aşırı yüklemek
     //int number : parametredir
-    public static String  voidsizParametreli(int number ) {
-        return number+"";
+    public static String voidsizParametreli(int number) {
+
+        return number + "";
     }
 
     //access modifier
@@ -39,6 +42,10 @@ public class _01_Method {
         //"Hamit" ==> argümandır
         System.out.println(voidsizParametreli("Hamit"));
         System.out.println(voidsizParametreli(44));
+
+        _02_AccessModifier accessModifier=new _02_AccessModifier();
+       // accessModifier.
+
 
 
     }
