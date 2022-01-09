7ffafd171509ce1bf9be8540c3ea6a5b6f1aa780
hamitmizrak <hamitmizrak@gmail.com>
2022-01-09 11:42:40 +0300

    konular

---
+++ src/com/ecodation/ders004/software/term/SoftwareTerm.java
@@ -0,0 +1,46 @@
+package com.ecodation.ders004.software.term;
+
+public class SoftwareTerm  {
+
+    //access modifier ==> public,protected, private ,   -
+    //oop
+    //class
+    //constructor
+    //toString
+    //getter setter
+    //hashcode()
+    //@Override
+    //static
+    //final
+    //this
+    //super
+    //instanceof
+    //Overloading
+    //lombok
+
+
+    //template import
+    //template sildiğimde otomatik silmesi
+    //lombok  bakalaım aktif edeceğiz
+
+    //Dummy codes
+    //spagetti codes
+    //clean codes
+    //kiss
+    //dry
+    //yagni
+    //ccp
+    //monad
+    //agile
+    //scrum
+    //mvc
+    //jira
+    //katmanlı mimari
+    //bean
+    //annotation
+
+
+
+    //solid
+
+}
